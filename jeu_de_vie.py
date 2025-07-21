a8fb17d13ffbe5c59baa57e23cb28cd210ed323a
Yakine <rachediyakine@gmail.com>
2025-07-21 13:49:44 +0200

    Point 5

--- jeu_de_vie.py
+++ jeu_de_vie.py
@@ -1,71 +1,42 @@
 import pygame as pg
 import numpy as np
 from scipy.signal import convolve2d
+from scipy.ndimage import gaussian_filter
+
+def gauss(x, mu, sigma) : 
+    return np.exp(-0.5*((x-mu)/sigma)**2)
 
 class Grille:
-    def __init__(self, dim, init_pattern=None, color_life=pg.Color("black"), color_dead=pg.Color("white")):
-        import random
-        self.dimensions = dim
-        if init_pattern is not None:
-            self.cells = np.zeros(self.dimensions, dtype=np.uint8)
-            indices_i = [v[0] for v in init_pattern]
-            indices_j = [v[1] for v in init_pattern]
-            self.cells[indices_i, indices_j] = 1
-        else:
-            self.cells = np.random.randint(2, size=dim, dtype=np.uint8)
-        self.col_life = color_life
-        self.col_dead = color_dead
+    def __init__(self, radius : int, mu = 0.5, sigma = 0.15, color_life=pg.Color("black"), color_dead=pg.Color("white")):
+    	#On crée un nouveau init qui a pour but d'adopter les anneaux, gauss etc
+        y, x = np.ogrid[-radius: radius, -radius : radius]
+        distance = np.sqrt((1+x)**2+(1+y)**2)/radius
+        self.kernel = gauss(distance, mu, sigma)
+        self.kernel[distance> 1] = 0 
+        self.kernel /= np.sum(self.kernel) 
+ 
+    def accroissement(self,x, mu = 0.15, sigma = 0.015) : 
+    	return -1 + 2 * gauss(x,mu, sigma)
 
+    def compute_next_iteration(self, cells, dt = 0.1):
+    	energy = convolve2d(cells, self.kernel, mode='same', boundary='wrap')
+    	acc = self.accroissement(energy)
+    	cells = np.clip(cells + dt *acc, 0, 1)
+    	return cells
 
-    def h(self,x) : 
-    	return np.where((x <= 1) | (x >= 4), -1, np.where(x == 2, 0, 1))
+class Drawing:
+#Nouveau Drawing
+    def __init__(self, width = 800, height = 600):
+        self.colors = np.array([np.ogrid[0.:255.:256j], np.ogrid[0.:255.:256j], np.ogrid[0.:255.:256j]]).T
+        self.dimensions = (width, height)
+        self.screen = pg.display.set_mode(self.dimensions)
 
-    def compute_next_iteration(self):
-        # Noyau de convolution pour compter les voisins
-        kernel = np.array([[1, 1, 1],
-                            [1, 0, 1],
-                            [1, 1, 1]])
-
-        # Calcul du nombre de voisins vivants pour chaque cellule
-        voisins = convolve2d(self.cells, kernel, mode='same', boundary='wrap')
-        
-        # Applifation de h a chaque cellule 
-        h_values = self.h(voisins)
-        
-        # Addition entre cell et h avec clip
-        next_cells = np.clip(self.cells + h_values, 0, 1)
-        
-        # Mise à jour de la grille
-        self.cells = next_cells
-
-class App:
-    def __init__(self, geometry, grid):
-        self.grid = grid
-        self.size_x = geometry[1] // grid.dimensions[1]
-        self.size_y = geometry[0] // grid.dimensions[0]
-        if self.size_x > 4 and self.size_y > 4:
-            self.draw_color = pg.Color('lightgrey')
-        else:
-            self.draw_color = None
-        self.width = grid.dimensions[1] * self.size_x
-        self.height = grid.dimensions[0] * self.size_y
-        self.screen = pg.display.set_mode((self.width, self.height))
-        self.canvas_cells = []
-
-    def compute_rectangle(self, i: int, j: int):
-        return (self.size_x * j, self.height - self.size_y * (i + 1), self.size_x, self.size_y)
-
-    def compute_color(self, i: int, j: int):
-        if self.grid.cells[i, j] == 0:
-            return self.grid.col_dead
-        else:
-            return self.grid.col_life
-
-    def draw(self):
-        [self.screen.fill(self.compute_color(i, j), self.compute_rectangle(i, j)) for i in range(self.grid.dimensions[0]) for j in range(self.grid.dimensions[1])]
-        if self.draw_color is not None:
-            [pg.draw.line(self.screen, self.draw_color, (0, i * self.size_y), (self.width, i * self.size_y)) for i in range(self.grid.dimensions[0])]
-            [pg.draw.line(self.screen, self.draw_color, (j * self.size_x, 0), (j * self.size_x, self.height)) for j in range(self.grid.dimensions[1])]
+    def draw(self, cells):
+        indices = (255*cells).astype(dtype=np.int32)
+        surface = pg.surfarray.make_surface(self.colors[indices.T])
+        surface = pg.transform.flip(surface, False, True)
+        surface = pg.transform.scale(surface, self.dimensions)
+        self.screen.blit(surface, (0,0))
         pg.display.update()
 
 if __name__ == '__main__':
@@ -73,46 +44,30 @@ if __name__ == '__main__':
     import sys
 
     pg.init()
-    dico_patterns = {
-        'blinker': ((5, 5), [(2, 1), (2, 2), (2, 3)]),
-        'toad': ((6, 6), [(2, 2), (2, 3), (2, 4), (3, 3), (3, 4), (3, 5)]),
-        "acorn": ((100, 100), [(51, 52), (52, 54), (53, 51), (53, 52), (53, 55), (53, 56), (53, 57)]),
-        "beacon": ((6, 6), [(1, 3), (1, 4), (2, 3), (2, 4), (3, 1), (3, 2), (4, 1), (4, 2)]),
-        "boat": ((5, 5), [(1, 1), (1, 2), (2, 1), (2, 3), (3, 2)]),
-        "glider": ((100, 90), [(1, 1), (2, 2), (2, 3), (3, 1), (3, 2)]),
-        "glider_gun": ((200, 100), [(51, 76), (52, 74), (52, 76), (53, 64), (53, 65), (53, 72), (53, 73), (53, 86), (53, 87), (54, 63), (54, 67), (54, 72), (54, 73), (54, 86), (54, 87), (55, 52), (55, 53), (55, 62), (55, 68), (55, 72), (55, 73), (56, 52), (56, 53), (56, 62), (56, 66), (56, 68), (56, 69), (56, 74), (56, 76), (57, 62), (57, 68), (57, 76), (58, 63), (58, 67), (59, 64), (59, 65)]),
-        "space_ship": ((25, 25), [(11, 13), (11, 14), (12, 11), (12, 12), (12, 14), (12, 15), (13, 11), (13, 12), (13, 13), (13, 14), (14, 12), (14, 13)]),
-        "die_hard": ((100, 100), [(51, 57), (52, 51), (52, 52), (53, 52), (53, 56), (53, 57), (53, 58)]),
-        "pulsar": ((17, 17), [(2, 4), (2, 5), (2, 6), (7, 4), (7, 5), (7, 6), (9, 4), (9, 5), (9, 6), (14, 4), (14, 5), (14, 6), (2, 10), (2, 11), (2, 12), (7, 10), (7, 11), (7, 12), (9, 10), (9, 11), (9, 12), (14, 10), (14, 11), (14, 12), (4, 2), (5, 2), (6, 2), (4, 7), (5, 7), (6, 7), (4, 9), (5, 9), (6, 9), (4, 14), (5, 14), (6, 14), (10, 2), (11, 2), (12, 2), (10, 7), (11, 7), (12, 7), (10, 9), (11, 9), (12, 9), (10, 14), (11, 14), (12, 14)]),
-        "floraison": ((40, 40), [(19, 18), (19, 19), (19, 20), (20, 17), (20, 19), (20, 21), (21, 18), (21, 19), (21, 20)]),
-        "block_switch_engine": ((400, 400), [(201, 202), (201, 203), (202, 202), (202, 203), (211, 203), (212, 204), (212, 202), (214, 204), (214, 201), (215, 201), (215, 202), (216, 201)]),
-        "u": ((200, 200), [(101, 101), (102, 102), (103, 102), (103, 101), (104, 103), (105, 103), (105, 102), (105, 101), (105, 105), (103, 105), (102, 105), (101, 105), (101, 104)]),
-        "flat": ((200, 400), [(80, 200), (81, 200), (82, 200), (83, 200), (84, 200), (85, 200), (86, 200), (87, 200), (89, 200), (90, 200), (91, 200), (92, 200), (93, 200), (97, 200), (98, 200), (99, 200), (106, 200), (107, 200), (108, 200), (109, 200), (110, 200), (111, 200), (112, 200), (114, 200), (115, 200), (116, 200), (117, 200), (118, 200)])
-    }
-    choice = 'glider_gun'
-    if len(sys.argv) > 1:
-        choice = sys.argv[1]
-    resx = 800
-    resy = 800
-    if len(sys.argv) > 3:
-        resx = int(sys.argv[2])
-        resy = int(sys.argv[3])
-    print(f"Pattern initial choisi : {choice}")
-    print(f"resolution ecran : {resx, resy}")
-    try:
-        init_pattern = dico_patterns[choice]
-    except KeyError:
-        print("No such pattern. Available ones are:", dico_patterns.keys())
-        exit(1)
-    grid = Grille(*init_pattern)
-    appli = App((resx, resy), grid)
+    
+    #N = 512
+    #M = int(np.ceil((16*N)/9))
+    # Gaussian spot centered in the middle
+    #radius = 36
+    #y, x = np.ogrid[-N//2:N//2, -M//2:M//2]
+    #cells = np.exp(-0.5 * (x*x + y*y) / (radius*radius))
+    N = 256
+    M = int(np.ceil((16*N)/9))
+
+    orbium = np.array([[0,0,0,0,0,0,0.1,0.14,0.1,0,0,0.03,0.03,0,0,0.3,0,0,0,0], [0,0,0,0,0,0.08,0.24,0.3,0.3,0.18,0.14,0.15,0.16,0.15,0.09,0.2,0,0,0,0], [0,0,0,0,0,0.15,0.34,0.44,0.46,0.38,0.18,0.14,0.11,0.13,0.19,0.18,0.45,0,0,0], [0,0,0,0,0.06,0.13,0.39,0.5,0.5,0.37,0.06,0,0,0,0.02,0.16,0.68,0,0,0], [0,0,0,0.11,0.17,0.17,0.33,0.4,0.38,0.28,0.14,0,0,0,0,0,0.18,0.42,0,0], [0,0,0.09,0.18,0.13,0.06,0.08,0.26,0.32,0.32,0.27,0,0,0,0,0,0,0.82,0,0], [0.27,0,0.16,0.12,0,0,0,0.25,0.38,0.44,0.45,0.34,0,0,0,0,0,0.22,0.17,0], [0,0.07,0.2,0.02,0,0,0,0.31,0.48,0.57,0.6,0.57,0,0,0,0,0,0,0.49,0], [0,0.59,0.19,0,0,0,0,0.2,0.57,0.69,0.76,0.76,0.49,0,0,0,0,0,0.36,0], [0,0.58,0.19,0,0,0,0,0,0.67,0.83,0.9,0.92,0.87,0.12,0,0,0,0,0.22,0.07], [0,0,0.46,0,0,0,0,0,0.7,0.93,1,1,1,0.61,0,0,0,0,0.18,0.11], [0,0,0.82,0,0,0,0,0,0.47,1,1,0.98,1,0.96,0.27,0,0,0,0.19,0.1], [0,0,0.46,0,0,0,0,0,0.25,1,1,0.84,0.92,0.97,0.54,0.14,0.04,0.1,0.21,0.05], [0,0,0,0.4,0,0,0,0,0.09,0.8,1,0.82,0.8,0.85,0.63,0.31,0.18,0.19,0.2,0.01], [0,0,0,0.36,0.1,0,0,0,0.05,0.54,0.86,0.79,0.74,0.72,0.6,0.39,0.28,0.24,0.13,0], [0,0,0,0.01,0.3,0.07,0,0,0.08,0.36,0.64,0.7,0.64,0.6,0.51,0.39,0.29,0.19,0.04,0], [0,0,0,0,0.1,0.24,0.14,0.1,0.15,0.29,0.45,0.53,0.52,0.46,0.4,0.31,0.21,0.08,0,0], [0,0,0,0,0,0.08,0.21,0.21,0.22,0.29,0.36,0.39,0.37,0.33,0.26,0.18,0.09,0,0,0], [0,0,0,0,0,0,0.03,0.13,0.19,0.22,0.24,0.24,0.23,0.18,0.13,0.05,0,0,0,0], [0,0,0,0,0,0,0,0,0.02,0.06,0.08,0.09,0.07,0.05,0.01,0,0,0,0,0]])
+    cells = np.zeros((N,M))
+    pos_x = M//6
+    pos_y = N//6
+    cells[pos_x:(pos_x + orbium.shape[1]), pos_y:(pos_y + orbium.shape[0])] = orbium.T    
+    grid = Grille( radius = 13)
+    appli = Drawing()
 
     mustContinue = True
     while mustContinue:
         t1 = time.time()
-        grid.compute_next_iteration()
+        cells = grid.compute_next_iteration(cells = cells)
         t2 = time.time()
-        appli.draw()
+        appli.draw(cells)
         t3 = time.time()
         for event in pg.event.get():
             if event.type == pg.QUIT:
